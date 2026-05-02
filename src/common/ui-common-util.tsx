54afb0600688ea73b8b6a5bdd3b0bba5d02a744a
H.M.Touhid Mia (HMTMCSE) <hmtm.cse@gmail.com>
2026-05-03 02:20:48 +0600

    Added Input Field

--- src/common/ui-common-util.tsx
+++ src/common/ui-common-util.tsx
@@ -4,16 +4,16 @@ export const UICommonUtil = {
     notImplemented(name: string, postMessage: string = "not implemented") {
         return (<>{name} {postMessage}</>)
     },
-    extractGridItemProps(props: Map<string, MixType>) {
+    extractGridItemProps(props: Record<string, MixType>) {
         const gridItemPropsKeys: Array<string> = ["colSpan", "rowSpan", "colStart", "colEnd", "rowStart", "rowEnd", "colSpanMob", "colSpanTab", "colSpanLarge", "rowSpanMob", "rowSpanTab", "rowSpanLarge", "colStartMob", "colStartTab", "colStartLarge", "colEndMob", "colEndTab", "colEndLarge", "rowStartMob", "rowStartTab", "rowStartLarge", "rowEndMob", "rowEndTab", "rowEndLarge"]
-        const gridItemProps: Map<string, MixType> = new Map<string, MixType>()
-        const otherProps: Map<string, MixType> = new Map<string, MixType>()
+        const gridItemProps: Record<string, MixType> = {}
+        const otherProps: Record<string, MixType> = {}
 
         Object.entries(props).forEach(([k, v]) => {
             if (gridItemPropsKeys.includes(k)) {
-                gridItemProps.set(k, v)
+                gridItemProps[k] = v
             } else {
-                otherProps.set(k, v)
+                otherProps[k] = v
             }
         })
 
