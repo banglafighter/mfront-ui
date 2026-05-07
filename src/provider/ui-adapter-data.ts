4120c1dc37a5aee3b798dbdf480eb930fccc5362
H.M.Touhid Mia (HMTMCSE) <hmtm.cse@gmail.com>
2026-05-07 12:52:40 +0600

    Working on various conditions

--- src/provider/ui-adapter-data.ts
+++ src/provider/ui-adapter-data.ts
@@ -1,8 +1,12 @@
 import {UIComponentSpec} from "./ui-component-spec";
 import {UIActionSpec} from "./ui-action-spec";
+import {UIComponent} from "mmcore";
 
 
 export interface UIAdapterData {
     component: UIComponentSpec
     action: UIActionSpec
+
+    customComponent: Record<string, UIComponent<unknown>>
+    customAction: Record<string, UIComponent<unknown>>
 }
