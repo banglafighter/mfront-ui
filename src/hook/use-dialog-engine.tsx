7b6569720218166fd28a46cc766810bb77f859d4
H.M.Touhid Mia (HMTMCSE) <hmtm.cse@gmail.com>
2026-05-12 17:38:43 +0600

    Working on Dialog

--- src/hook/use-dialog-engine.tsx
+++ src/hook/use-dialog-engine.tsx
@@ -1,14 +1,40 @@
-import {DialogSlideFrom, WebDialogEngineProps} from "mmcore-ui";
+import {DialogEngineOpenProps, WebDialogEngineProps, WebFieldSpec} from "mmcore-ui";
+import {mmReactUseRef, mmReactUseState, UINode} from "mmcore";
 
 export default function useDialogEngine(): WebDialogEngineProps {
-    const open = (title?: string, subTitle?: string, slideFrom?: DialogSlideFrom) => {
+    let actionData = mmReactUseRef<Record<string, UINode>>({})
+    const [isOpen, setOpen] = mmReactUseState(false);
+    const open = (props: DialogEngineOpenProps) => {
+        actionData.current = {}
+        const _actionData = actionData.current
+        if (props.title) {
+            _actionData.title = props.title
+        }
+        if (props.subTitle) {
+            _actionData.subTitle = props.subTitle
+        }
 
+        if (props.slideFrom) {
+            _actionData.slideFrom = props.slideFrom
+        }
+        setOpen(true);
     }
+
     const close = () => {
+        setOpen(false);
+    }
+
+    const getActionValue = (dataKey: string, defaultData?: UINode): UINode => {
+        if (Object.prototype.hasOwnProperty.call(actionData.current, dataKey)) {
+            return actionData.current[dataKey];
+        }
+        return defaultData;
     }
 
     return {
         close,
         open,
+        isOpen,
+        getActionValue
     }
 }
